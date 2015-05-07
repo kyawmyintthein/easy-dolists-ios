2a61d2fd6f8ff5843462a5f72ca89093476cb47e
Kyaw Myint Thein <kyawmyintthein2020@gmail.com>
2015-05-07 20:03:24 +0800

    Initial Commit

---
+++ EasyDoLists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyDoLists
+//
+//  Created by Kyaw Myint Thein on 5/7/15.
+//  Copyright (c) 2015 com.easydolists. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EasyDoLists/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EasyDoLists
+//
+//  Created by Kyaw Myint Thein on 5/7/15.
+//  Copyright (c) 2015 com.easydolists. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
